62f243ad1ae10420966e34b0bf074f1855348aa8
Chris Drain <DrainC4@yahoo.com>
2017-07-27 07:12:11 -0700

    fix

--- nameParameters.php
+++ nameParameters.php
@@ -5,7 +5,7 @@ $stmt = $conn->prepare('select * from test where username = :username');
 $name = 'chris';
 
 $stmt->bindParam(':username', $name);
-//OptionalPDO::Param_STR $stmt->bindValue(':username', $name, PDO::PARAM_STR);
+//Optional PDO::Param_STR $stmt->bindValue(':username', $name, PDO::PARAM_STR);
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "<h2>".htmlentities($row['username']). " ".htmlentities($row['password']) . "</h2>";
--- updateRecords.php
+++ updateRecords.php
@@ -1,7 +1,7 @@
 <?php
 include ('index.php');
 
-//$stmt = $conn->prepare('delet from test where username = :username');
+//$stmt = $conn->prepare('delete from test where username = :username');
 $stmt= $conn->prepare('update test set password = :password where username = :username');
 $stmt->bindValue(':username', $_POST['username']);
 $stmt->bindValue(':password', $_POST['password']);
